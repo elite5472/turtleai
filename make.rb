9f91d2f27ea0fb485077ddbeb5a07d8ea018615e
elite5472 <guiborges@live.com>
2015-08-20 16:07:25 -0300

    Updated readme

--- make.rb
+++ make.rb
@@ -48,6 +48,8 @@ Turtle AI
 
 Turtle AI is a project to develop turtles that leverage the BDI (Beliefs, Desires, Intentions) planning model to aid the player with numerous tasks.
 
+Link to Research Paper (In progress): https://www.overleaf.com/read/qvjhxqfxdywk
+
 The project uses make.rb to dynamically prepare lua scripts for use within ComputerCraft. The pastebin commands bellow are dynamically updated.
 
 ```yaml
