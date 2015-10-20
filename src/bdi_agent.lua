3536fe5f62b69cc416b02f4e8f2b934daf908070
Guillermo Borges <guiborges@live.com>
2015-10-19 22:31:14 -0200

    latest

--- src/bdi_agent.lua
+++ src/bdi_agent.lua
@@ -95,6 +95,7 @@ BDI_Agent = Agent:new({
 						if dir == nil then 
 							fail = true
 						else
+							print("s1 " .. fail)
 							fail = not (agent:turn_to(dir) and (agent:move_forward() or (agent:dig_forward() and agent:move_forward())))
 						end
 					end
