3ced591b8136fb15ad26a881e28414961949e0ee
Guillermo Borges <guiborges@live.com>
2015-10-19 22:54:01 -0200

    latest

--- src/bdi_agent.lua
+++ src/bdi_agent.lua
@@ -96,7 +96,7 @@ BDI_Agent = Agent:new({
 						if dir == nil then 
 							fail = true
 						else
-							print(s1)
+							print("s1")
 							fail = not (agent:turn_to(dir) and (agent:move_forward() or (agent:dig_forward() and agent:move_forward())))
 							print(fail)
 						end
@@ -164,6 +164,7 @@ BDI_Agent = Agent:new({
 		elseif d:opposite() == direction then
 			return self:turn_around()
 		end
+		print("No direction")
 	end;
 	
 	update_knowledge = function(self)
