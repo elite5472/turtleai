9c15a53f8a809fc4664623c7248b58080bbb65a6
Guillermo Borges <guiborges@live.com>
2015-10-19 22:20:56 -0200

    latest

--- src/bdi_agent.lua
+++ src/bdi_agent.lua
@@ -48,7 +48,7 @@ BDI_Agent = Agent:new({
 			estimate = function(a, b)
 				local d = b - a
 				local distance = math.abs(d.x) + math.abs(d.y) + math.abs(d.z)
-				return 1.5 * distance
+				return 2 * distance
 			end;
 	
 			expand = function(a)
