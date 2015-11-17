dc4c8f8f7a404386c97dadadb4f8df4bec85c7f1
Guillermo Borges <guiborges@live.com>
2015-11-16 23:08:24 -0200

    Repository fixes

--- app/bditest.lua
+++
@@ -1,7 +0,0 @@
-require "bdi_agent"
-require "blua"
-a = BDI_Agent:new({})
-engine = Blua:new({ agent = a })
-while true do
-	print(engine:step())
-end
--- src/class.lua
+++
@@ -1,8 +0,0 @@
-Class = {
-	new = function(self, o)
-		o = o or {}
-		setmetatable(o, self)
-		self.__index = self
-		return o
-	end;
-}
